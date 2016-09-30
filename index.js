007201a9d5ab8b01d3254a61a1404d548d9e755c
Isaac Murchie <isaac@saucelabs.com>
2016-09-30 19:55:33 -0400

    Move appium-ios-log into base-driver

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ export { BaseDriver, DeviceSettings };
 export default BaseDriver;
 
 // MJSONWP exports
-
 import { MobileJsonWireProtocol, routeConfiguringFunction, errors, isErrorType,
          errorFromCode, ALL_COMMANDS, NO_SESSION_ID_COMMANDS,
          isSessionCommand } from './lib/mjsonwp';
@@ -17,19 +16,21 @@ export { MobileJsonWireProtocol, routeConfiguringFunction, errors, isErrorType,
          errorFromCode, ALL_COMMANDS, NO_SESSION_ID_COMMANDS, isSessionCommand };
 
 // Express exports
-
 import { server } from './lib/express/server';
-
 export { server };
 
 // jsonwp-proxy exports
-
 import JWProxy from './lib/jsonwp-proxy/proxy';
-
 export { JWProxy };
 
 // jsonwp-status exports
-
 import { codes, getSummaryByCode } from './lib/jsonwp-status/status';
 const statusCodes = codes;
-export {statusCodes, getSummaryByCode };
+export { statusCodes, getSummaryByCode };
+
+// ios-log exports
+import IOSLog from './lib/ios-log/ios-log';
+import IOSCrashLog from './lib/ios-log/ios-crash-log';
+import IOSPerformanceLog from './lib/ios-log/ios-performance-log';
+
+export { IOSLog, IOSCrashLog, IOSPerformanceLog };
